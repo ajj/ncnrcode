d1616c5279931280b4736a0dccf523c9a1bbbd2b
Andrew Jackson <andrew.jackson@esss.se>
2008-11-13 19:13:31 +0000

    git-svn-id: http://danse.chem.utk.edu/svn/sans/utils/bt5@446 8e55283c-c6c9-4f4c-b3dc-704b0a22af0c

--- bt5plot2/usans.py
+++ bt5plot2/usans.py
@@ -21,41 +21,52 @@ def getBT5DataFromFile(fileName):
     detdata = {}
     metadata = {}
     motlist = []
-    
+
+    print "File: ",fileName    
     inputfile = open(fileName, "r")
 
     inputdata = inputfile.readlines()
 
-    mdtmp = inputdata[0].replace("'","")
-    mdtmp = mdtmp.split()
-    
-    #Sundry metadata about run settings
-    (metadata['filename'], metadata['datetime'],
-    metadata['mon'],metadata['prefactor'],
-    metadata['base'],metadata['numpnts'],
-    metadata['type']) = (mdtmp[0],' '.join(mdtmp[1:5]),float(mdtmp[6]),int(mdtmp[7]),mdtmp[8],int(mdtmp[9]),mdtmp[10])
-    
-    #Comment string
-    metadata['title'] = inputdata[2].strip()
-    
-    #Start, step and end values for motors 1-6
-    motlist.append(inputdata[5].split()[1:])
-    motlist.append(inputdata[6].split()[1:])
-    motlist.append(inputdata[7].split()[1:])
-    motlist.append(inputdata[8].split()[1:])
-    motlist.append(inputdata[9].split()[1:])
-    motlist.append(inputdata[10].split()[1:]) 
-    metadata['motorvals'] = motlist
-    
-    for index in range(13, len(inputdata), 2):
-        detdata[float(inputdata[index].split()[0])] = inputdata[index + 1].split(',')    
+    if len(inputdata) < 2:
+        inputfile.close()
+        return 0,0
 
-    for key in detdata.keys():
-        for val in range(0, len(detdata[key])):
-            detdata[key][val] = int(detdata[key][val])
+    if inputdata[1].find('Filename') > 0:
 
-    inputfile.close()
-    return detdata,metadata
+        mdtmp = inputdata[0].replace("'","")
+        mdtmp = mdtmp.split()
+    
+        #Sundry metadata about run settings
+        (metadata['filename'], metadata['datetime'],
+        metadata['mon'],metadata['prefactor'],
+        metadata['base'],metadata['numpnts'],
+        metadata['type']) = (mdtmp[0],' '.join(mdtmp[1:5]),float(mdtmp[6]),int(mdtmp[7]),mdtmp[8],int(mdtmp[9]),mdtmp[10])
+    
+        #Comment string
+        metadata['title'] = inputdata[2].strip()
+    
+        #Start, step and end values for motors 1-6
+        motlist.append(inputdata[5].split()[1:])
+        motlist.append(inputdata[6].split()[1:])
+        motlist.append(inputdata[7].split()[1:])
+        motlist.append(inputdata[8].split()[1:])
+        motlist.append(inputdata[9].split()[1:])
+        motlist.append(inputdata[10].split()[1:]) 
+        metadata['motorvals'] = motlist
+    
+        for index in range(13, len(inputdata), 2):
+            detdata[float(inputdata[index].split()[0])] = inputdata[index + 1].split(',')    
+
+        for key in detdata.keys():
+            for val in range(0, len(detdata[key])):
+                detdata[key][val] = int(detdata[key][val])
+
+        inputfile.close()
+        return detdata,metadata
+
+    else:
+        inputfile.close()
+        return 0,0
 
 def printBT5DetData(detdata):
     '''
@@ -68,7 +79,7 @@ def printBT5DetData(detdata):
     motorvals.sort(cmp=numeric_compare)
 
     for motorval in motorvals:
-        str = motorval + ":"
+        str = repr(motorval) + ":"
         str += "\tMon: " + repr(detdata[motorval][0])
         str += "\tDet 1-5: " + repr(detdata[motorval][2])
         str += "\t" + repr(detdata[motorval][1])
